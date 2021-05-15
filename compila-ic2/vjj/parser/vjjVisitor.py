b753d6148739768f1bdf0732cf09dd8cbd6520bc
vinicius pizetta de souza <viniciuspzt@gmail.com>
2021-05-15 17:12:39 -0300

    update

--- compila-ic2/vjj/parser/vjjVisitor.py
+++ compila-ic2/vjj/parser/vjjVisitor.py
@@ -3,7 +3,7 @@ from antlr4 import *
 if __name__ is not None and "." in __name__:
     from .vjjParser import vjjParser
 else:
-    from vjjParser import vjjParser
+    from .vjjParser import vjjParser
 
 # This class defines a complete generic visitor for a parse tree produced by vjjParser.
 
